fccaf24fb5f7d2093b5c3277d85aa884aa9e02f6
Joseph W Donahue <joseph@sharperhacksllc.com>
2025-03-20 23:28:53 -0700

    NB: Updated solution file and test dependencies.

--- build.cmd
+++ build.cmd
@@ -12,7 +12,7 @@
 @if /i "%1" equ "--help" (@goto ShowHelp)
 
 @set _buildType=Debug
-@set _solution=StringExtensions.sln
+@set _solution=StringExtensions.slnx
 @set _buildVerbosity=minimal
 @set _testVerbosity=minimal
 @call :SetVersionVariables
